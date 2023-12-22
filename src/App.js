290a9f34f7b2369ed981f565770b0c85d9ed71e6
pitersizovdev <pitersizov@gmail.com>
2023-12-23 01:36:47 +0300

    0.0.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>SIZOV</code> 4MD15.
         </p>
         <a
           className="App-link"
